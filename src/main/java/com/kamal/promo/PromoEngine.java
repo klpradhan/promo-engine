1fbff99a0a3199a27b180b6d419c92d41a7044fc
Kamal <30713961+klpradhan@users.noreply.github.com>
2022-10-23 06:59:22 +0530

    Intial test case template

---
+++ src/main/java/com/kamal/promo/PromoEngine.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.kamal.promo;
+
+/**
+ * Promo Engine
+ * Calculate promo of given skus in the cart and return the total order value
+ * 
+ */
+public class PromoEngine {
+
+	public double calculateTotalOrderValue() {
+		double orderValue = 0.0;
+		return orderValue;
+	}
+}
---
+++ src/test/java/com/kamal/promo/PromoEngineTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.kamal.promo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Promo Engine Unit test cases
+ *
+ */
+class PromoEngineTest {
+
+	@BeforeEach
+	void init() throws Exception {
+	}
+
+	@Test
+	@DisplayName("FIXED_A_3_130")
+	@Disabled
+	void FIXED_A_3_130() {
+		fail("Not yet implemented");
+	}
+
+}
